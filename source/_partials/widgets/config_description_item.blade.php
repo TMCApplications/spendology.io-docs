9cc6e3c27a68c4e06aba1274c01f72b304c23b52
dmason30 <danmason@tmcapplications.co.uk>
2019-06-17 01:23:58 +0100

    wip

--- source/_partials/widgets/config_description_item.blade.php
+++ source/_partials/widgets/config_description_item.blade.php
@@ -2,7 +2,13 @@
     <h5 id="{{$group}}-type-{{$type}}"><strong>{{ucfirst($type)}}</strong></h5>
     @foreach($properties as $propertyName => $property)
         <div>
-            <h6 id="{{$group}}_{{$type}}_{{$propertyName}}" class="font-bold mb-0 {{$property->recommended ? 'recommended' : ''}}">{{$property->displayName}}</h6>
+            <h6
+                id="{{$group}}_{{$type}}_{{$propertyName}}"
+                class="inline font-bold mb-0 {{$property->recommended ? 'recommended' : ''}}"
+            >
+                {{$property->displayName}}
+            </h6>
+            <br />
             <small>
                 <strong>Property:</strong> <code>{{$propertyName}}</code>
             </small>
